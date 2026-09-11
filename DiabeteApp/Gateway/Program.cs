0ad7a9cb05be2c54a7b9af28c96901e7b28fbabb
S-Marnat <242020513+S-Marnat@users.noreply.github.com>
2026-09-11 16:37:10 +0200

    feat: mise en place d'Ocelot avec sa structure minimale

--- DiabeteApp/Gateway/Program.cs
+++ DiabeteApp/Gateway/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.AspNetCore.Identity;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Configuration d'Ocelot
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+    .AddCookie(IdentityConstants.ApplicationScheme);
+builder.Services.AddAuthorization();
+
+builder.Services.AddOcelot();
+
+
+// Construction de l'API
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+// Pipeline HTTP
+app.UseAuthentication();
+app.UseAuthorization();
+
+await app.UseOcelot();
 
 app.Run();
